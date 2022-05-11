368b6a49bdb1f846724ea2328811b49e08db1019
CSI499 <92383690+CSI499@users.noreply.github.com>
2022-05-11 10:09:59 -0400

    update webpage

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,8 +12,7 @@ export default function About() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              text='Hi there, my name is Dan, welcome to my personal webpage. I recently graduated from SUNY at Albany this past December and currently do freelance web development. When I’m not programming I have a multitude of different interests including playing guitar, hiking, rock climbing, painting, and growing fresh vegetables every summer. I have worked numerous roles before going back to finish my degree in Computer Science which has given me a unique insight on numerous topics ranging from environmental protection to audio studio equipment. I believe this has helped to make me a more well rounded individual and I’d love to share my creativity and work ethic to you!'
-              label='About'
+              text='Hi there, welcome to my personal webpage :) My name is Dan, I’m a recent graduate of SUNY at Albany where I completed a Bachelor’s of Science in Computer Science. Since graduation I’ve been working as a freelance web developer creating webpages for local artists and businesses using the React framework. I’m also continuing development of my own Java finance application utilizing JavaFX and MySQL. When I’m not programming I have a multitude of different interests that include; playing guitar, hiking, rock climbing, painting, and growing tomatoes. I have worked in multiple roles over the years in a management position and I believe this has given me a unique incite on the needs of a business and how to best help deliver them. I’d love to share my passion for creating and work ethic with you so feel free to reach out!'
               path='/about'
             />
           </ul>
